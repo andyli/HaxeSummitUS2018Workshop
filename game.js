a6704f6909f1f08327b54c5dbfe13a638e17097d
Andy Li <andy@onthewings.net>
2018-05-04 17:04:17 +0000

    deploy for c163d875a92bf640a4de6ac00df6cf4bb457d177

--- game.js
+++ game.js
@@ -335,7 +335,7 @@ Main.main = function() {
 Main.__super__ = hxd_App;
 Main.prototype = $extend(hxd_App.prototype,{
 	init: function() {
-		haxe_Log.trace("built at " + "2018-05-04 15:13:54",{ fileName : "Main.hx", lineNumber : 28, className : "Main", methodName : "init"});
+		haxe_Log.trace("built at " + "2018-05-04 17:04:02",{ fileName : "Main.hx", lineNumber : 28, className : "Main", methodName : "init"});
 		this.world = new game_World(this.s2d.width,this.s2d.height);
 		this.id = this.world.createPlayer().id;
 		this.stage.addEventTarget($bind(this,this.onEvent));
